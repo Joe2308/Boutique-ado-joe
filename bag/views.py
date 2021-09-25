a7e8c466f71c8bcedf423dcf6e6feb3879958715
Joe <joebreilly@yahoo.ie>
2021-09-25 15:05:31 +0000

    Adjust and remove products

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -34,3 +34,57 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Add the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    # Using session keeps the shopping bag live until the user closes their browser
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+
+    else:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+    try:
+        redirect_url = request.POST.get('redirect_url')
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        # Using session keeps the shopping bag live until the user closes their browser
+        bag = request.session.get('bag', {})
+
+        if size:
+            if quantity > 0:
+                del bag[item_id]['items_by_size'][size]
+                if not bag[item_id]['items_by_size']:
+                    bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
