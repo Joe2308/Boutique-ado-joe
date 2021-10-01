aaad6bda97ce9f5d85459f8bcf9a0e35c87f47e4
Joe <joebreilly@yahoo.ie>
2021-10-01 13:15:02 +0000

    Stripe error messages and secret and public keys

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+import stripe
 
 
 # Create your views here.
@@ -10,6 +13,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
