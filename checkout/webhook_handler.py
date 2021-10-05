d4d35ea06a1fb6c01cd1bfa3d8a4410807eae42d
Joe <joebreilly@yahoo.ie>
2021-10-05 20:45:21 +0000

    webhook url path updated in checkout/urls.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """ Handle stripe webhooks """
+    """Handle Stripe webhooks"""
 
-    def __inti__(self, request):
+    def __init__(self, request):
         self.request = request
 
     def handle_event(self, event):
@@ -23,7 +23,7 @@ class StripeWH_Handler:
             content=f'Webhook received: {event["type"]}',
             status=200)
 
-    def handle_payment_intent_failed(self, event):
+    def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
